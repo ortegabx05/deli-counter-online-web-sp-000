6b0d151ecbddd10321d98859e7a7b900c44d3ca9
jessi ortega <ortegabx05@gmail.com>
2019-07-08 18:07:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,16 @@
-deli_counter =[]
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
+
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
