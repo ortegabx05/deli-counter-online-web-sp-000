e5df993597531a6076c4853cca0891a61c528ff9
jessi ortega <ortegabx05@gmail.com>
2019-07-08 18:06:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,11 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
   return name, position 
 end 
+
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end
