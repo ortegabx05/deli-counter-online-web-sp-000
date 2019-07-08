6ce00cc60408ca9da93068b08629d0e1b9adb0ce
jessi ortega <ortegabx05@gmail.com>
2019-07-08 18:05:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
 deli_counter =[]
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
